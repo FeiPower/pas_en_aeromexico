45625db119a58fb42b06c01f7ecccd6984063d0e
FeiPower <38192118+FeiPower@users.noreply.github.com>
2025-05-21 12:46:31 -0600

    First commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// Inicializa el fondo animado de nubes usando VANTA.CLOUDS
+VANTA.CLOUDS({
+  el: "#clouds-bg",
+  mouseControls: false,
+  touchControls: false,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  skyColor: 0x68b8d7,
+  cloudColor: 0xadc1de,
+  cloudShadowColor: 0x6b7f91,
+  sunColor: 0xc56d19,
+  sunGlareColor: 0xff6633,
+  sunlightColor: 0xc07033,
+  speed: .80
+});
